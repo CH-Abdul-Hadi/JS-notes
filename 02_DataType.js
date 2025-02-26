aee7a6086087aa00b971be5a4016259a66fe3ffe
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-26 20:02:45 +0500

    strick standard of javaScript datatype

---
+++ 02_DataType.js
@@ -0,0 +1,27 @@
+// "use strick"; // use newer vision of javaScript
+// ECMA Script  // standard of javaScript
+
+// number 
+// range is 2 to power of 53;
+// bigInt is use for number greater than range now
+
+// string
+// to be use in quotes
+
+// boolean 
+// true/false
+
+// null
+// standalone value
+// typeof null is object
+
+// undefine
+// variable is declared but not define
+
+// symbol
+// use for uniqueness
+
+// object
+// use key value pairs
+
+// typeof => use to check type
