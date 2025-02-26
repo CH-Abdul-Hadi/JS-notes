d439eebb47c6e20d9ed62546a04f8121b3d1195a
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-26 20:21:07 +0500

    Math operations

---
+++ 04_operations.js
@@ -0,0 +1,12 @@
+// + // addition
+// - // subtraction
+// / //divide
+// * // multiply
+// ** //power of
+// % // modules
+
+// + concatenations
+
+// num++ post increment
+// ++num pre increment
+
