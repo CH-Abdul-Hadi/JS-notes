c3fcdf27d16818d4f8c6f959018cece496c50193
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-26 20:12:34 +0500

    conversion between datatype

---
+++ 03_conversion.js
@@ -0,0 +1,11 @@
+// Number() //convert anything to  datatype number
+// "33" => number
+// "33abc" => NaN
+// "aoa" => NaN
+// bool value => 0/1
+
+// Boolean() //convert anything to datatype boolean
+// empty,0,null =>false
+// others => true
+
+// String() //convert anything to  datatype string
