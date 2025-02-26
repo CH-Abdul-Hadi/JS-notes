5b45c37bfbedaa98c73ed8f8d67b09ac3f93e147
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-26 18:58:02 +0500

    variable use case different variable which to use and not

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId =122;
+// not allowed to change
+// block level
+let accountCity= "Peshawar";
+// Allowed to change
+// block level
+var accountEmail="abc@gmail.com";
+// Allowed to change
+// global level
+// prefer not to use var
+
