7ee76dd4bc96c0ed83be2e2c67fa6c43058a65bc
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-27 19:09:13 +0500

    part one of Basics

---
+++ 01_Basics/07_Strings.js
@@ -0,0 +1,17 @@
+// String 
+// can concatenate
+// use backticks for string manipulation
+// string is  an object as it use key value pairs
+
+// string methods
+// toUpperCase()
+// toLowerCase()
+// charAt()
+// indexOf()
+// length
+// subString() can use -num
+// Slice() can use -num
+// trim() remove space in string
+// replace() replace word in string
+// include() to check if it include something
+// split() make string an array at given position
---
+++ 01_Basics/08_Number_Math.js
@@ -0,0 +1,26 @@
+// 400 // output 400
+// Number(400 )// output Number:400
+
+// Number Method
+// toString() // convert number to string
+// length
+// toFixed() // give number in fixed decimal number
+// toPrecision() // btw 1-21, return String , priority is before decimal
+// toLocalSting() //add comas to number to usa standard, can pacify in which we want to convert
+
+// +++++++++++++++++++ MATHS +++++++++++++++++++
+
+// Math methods
+// abs() absolute value // change sign - to +
+// round() // change to round
+// floor() // round to lower value
+// ceil() // round to higher value
+// min() // give minimum value btw num
+// max() // give maximum value btw num
+// random() // values 0-1,
+
+// let min= 10
+// let max =20
+// Math.floor(Math.random() * (max - min + 1)) + min; to give value btw specific range
+
+
