844bd628fe9254468db2090891228dca50962cf8
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-02-27 20:04:40 +0500

    arrays finished objects started

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,31 @@
+// Arrays
+// type: object
+// let arr = [1,2,3,45,]
+// resizable
+// always starts form 0 index
+
+// +++++ Swallow copy
+// share same reference point 
+
+// +++++ deep copy
+// does not share same reference point 
+
+// methods
+// length
+// push() ------------ add to last index
+// pop() ------------- remove last index
+// unshift() --------- add to first index
+// shift() ----------- remove first index
+// include() --------- to check if it has value // return in boolean
+// indexOf() --------- give index of value
+// join() ------------ change type of array to string
+
+// slice() ----------- it does not modify the original array
+// splice() ---------- it modifies the original array.
+
+// concat() ---------- join to array
+
+// spared ..., [...arr1, ...arr2, ...arrN]
+// flat() remove array in array making it one array
+// isArray() tell if it array or not
+// from() ,make type to array
---
+++ 02_Basics/02_objects.js
@@ -0,0 +1,20 @@
+// objects singleton (object.create)
+
+// objects literals 
+/*
+let sym = symbol("key")
+
+ obj ={
+    name : "abdul",
+    age: 23,
+    sym: "key1" // type is string
+    [sym]: "key1" // type is symbol
+    location: "peshawar"
+} 
+    access
+    obj.name
+    obj["name"]
+
+    methods
+    obj.freeze(name)// cannot change name now
+*/
